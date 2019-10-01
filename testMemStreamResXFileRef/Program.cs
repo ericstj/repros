f2f2a6a092d7a61c9da8abe249b2162e75ed1587
Eric StJohn <ericstj@microsoft.com>
2019-10-01 09:43:34 -0700

    Add MemoryStreamResXFileRef repro

---
+++ testMemStreamResXFileRef/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace testMemStreamResXFileRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = Resources.test;
+            Console.WriteLine(test.GetType());
+
+            ResourceManager resourceManager = new ResourceManager("testMemStreamResXFileRef.Resources", typeof(Resources).Assembly);
+            var test2 = resourceManager.GetStream("test", CultureInfo.InvariantCulture);
+            Console.WriteLine(test2.GetType());
+        }
+    }
+
+}
